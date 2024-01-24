e2ec98fc54bc9ea531027b05ae0723e9456ea41a
NABIN777 <nirajan.khanal777@gmail.com>
2024-01-24 12:40:04 +0545

    deleted

--- apps/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path;
-from firstone import views;
-
-urlpatterns = [
-    path('',views.show_firstone,name='firstone'),
-    path('new',views.show_new)
-    
-]
--- apps/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def show_firstone(request):
-    return render(request,'firstone/firstone.html')
-    
