f60221600c4be9a17f31be3099942247f8b3ac99
NABIN777 <nirajan.khanal777@gmail.com>
2024-01-29 14:19:24 +0545

    error fix

---
+++ podtalk/lib/features/home/provider/favorite_provider.dart
@@ -0,0 +1,13 @@
+// import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// abstract class FavoriteNotifier extends Notifier<bool> {
+//   FavoriteNotifier() : super(false);
+
+//   void toggleFavorite() {
+//     state = !state;
+//   }
+// }
+
+// final favoriteProvider = NotifierProvider<FavoriteNotifier, bool>(
+//   (ref) => FavoriteNotifier(false),
+// );
